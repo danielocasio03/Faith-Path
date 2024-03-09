b6aa4bfb762e0d2c14b1b4d9884273389d0e11e6
danielocasio03 <64695744+danielocasio03@users.noreply.github.com>
2024-03-08 22:06:55 -0500

    *Created a VerseOTDModel to decode JSON data fetched from verse API *Created a VerseOTDFetchManager to handle fetching verse of the day from the API and pass it to the HomeViewController *Added functionality to HomeViewController to recieve fetched verse data and update VerseOTDView with said data *Imported photo assets and created a ChristianImages array in color manager to hold all of the images for use *Changed the VerseOTDView's ChristianImage property to get a new image from the ChristianImages array in ColorManager each time the app is loaded

---
+++ Praise Path/Model/VerseOTDFetchManager.swift
@@ -0,0 +1,8 @@
+//
+//  VerseOTDFetchManager.swift
+//  Praise Path
+//
+//  Created by Daniel Efrain Ocasio on 3/8/24.
+//
+
+import Foundation
---
+++ Praise Path/Model/VerseOTDModel.swift
@@ -0,0 +1,8 @@
+//
+//  VerseOTDModel.swift
+//  Praise Path
+//
+//  Created by Daniel Efrain Ocasio on 3/8/24.
+//
+
+import Foundation
